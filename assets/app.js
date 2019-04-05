0f9244dff24b6d4727ef3233ba54c6a436dadcb8
Ruslan <matveev.ruslan@gmail.com>
2019-04-05 09:37:25 +0300

    gitignore

---
+++ assets/app.js
@@ -0,0 +1,198 @@
+var Electron = require('electron'),
+	remote = Electron.remote,
+	ipcRenderer = Electron.ipcRenderer,
+	isOSX = (process.platform === 'darwin');
+
+var win = remote.getCurrentWindow()
+var nodeConsole = require('console');
+console = myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+
+// alert(document.body.style.width)
+
+var documentRef = $(document);
+
+
+
+
+documentRef.on('mousedown', function(event) {
+
+	var canvas = $('#canvas');
+	var sx = event.pageX, sy = event.pageY;
+
+
+	documentRef.on('mousemove.something', function(event) {
+
+		var x = Math.min(event.pageX, sx),
+			y = Math.min(event.pageY, sy),
+			w = Math.abs(event.pageX - sx),
+			h = Math.abs(event.pageY - sy);
+
+
+
+		canvas.show().css({
+			left: x,
+			top: y,
+			width: w,
+			height: h
+		})
+
+	// 	console.info(event.pageX, event.pageY)
+
+	// 	ctx.beginPath();
+	// 	ctx.lineWidth = 1;
+	// 	ctx.strokeStyle = "red";
+	// 	ctx.rect(5, 5, 290, 140); 
+	// 	ctx.stroke();
+	// });
+
+		documentRef.one('mouseup.something', function() {
+			canvas.hide();
+			documentRef.off('.something');
+
+		});
+
+	});
+
+
+});
+
+ipcRenderer.on('renderDesktop', function(event, files) {
+
+	var wrapper = $('.wrapper');
+
+	//wrapper.html(JSON.stringify(files));
+
+	for (var c = 0; c < files.length; c++) {
+		
+
+		
+		var el = $('<div/>')
+
+		.addClass('icon')
+
+		.data('path', files[c].path)
+
+		.attr('draggable', true)
+
+		.css({
+		})
+
+		.html(`
+			<img src="${files[c].icon}" />
+			<div>${files[c].name}</div>
+		`)
+
+		.appendTo(wrapper);
+	}
+
+
+
+	if (!isOSX) {
+		var f = window.location.search.split(/[?&=]/g);
+		document.body.style.width = f[2] + 'px';
+		document.body.style.height = f[4] + 'px';
+	}
+
+	(isOSX ? win.show() : win.showInactive());
+	if (!isOSX) setTimeout(() => win.setResizable(false), 500);
+});
+
+
+documentRef.on('click', '.quitButton', function() {
+	win.close();
+});
+
+documentRef.on('dblclick', '.icon', function() {
+	ipcRenderer.send('openItem', $(this).data('path'));
+});
+
+
+
+// documentRef.on('dragenter', function(event) {
+	
+// 	var target = $(event.target);
+	
+// 	if (!target.is('.icon')) {
+// 		target = target.closest('.icon');
+// 	}
+
+// 	if (target.length) {
+// 		target.css('background-color', 'red');
+// 	}
+// });
+
+// documentRef.on('dragleave', function(event) {
+// 	$( event.target).css('background-color', '');
+// });
+
+
+var currentTarget;
+
+
+documentRef.on('dragover', function(event) {
+
+	if (currentTarget) {
+		currentTarget.css('background-color', '');
+		console.info(1);
+	}
+	
+	event = event.originalEvent;
+	event.dataTransfer.dropEffect = 'none';
+	
+	var target = $(event.target);
+	
+	if (!target.is('.icon')) {
+		target = target.closest('.icon');
+	}
+
+	if (target.length) {
+		currentTarget = target;
+		event.dataTransfer.dropEffect = 'copy';
+		target.css('background-color', 'red');
+	}
+
+
+	event.preventDefault();
+	event.stopPropagation();
+	return false;
+
+
+
+	// myConsole.info(event.target.className);
+
+	// event.dataTransfer.dropEffect = 'move'
+
+	// myConsole.info(
+	// 	event.dataTransfer.files.length
+	// );
+
+
+	// event.preventDefault();
+});
+
+documentRef.on('drop', '.icon', function(event) {
+	event = event.originalEvent;
+	myConsole.info(
+		event.dataTransfer.files[0].path,
+		'->',
+		$(this).data('path')
+	);
+});
+
+
+documentRef.on('dragcancel', function() {
+	myConsole.info('aa');
+	// if (currentTarget) {
+	// 	currentTarget.css('background-color', '');
+	// 	currentTarget = null;
+	// }
+})
+
+
+documentRef.on('dragend', function() {
+	myConsole.info('x');
+	// if (currentTarget) {
+	// 	currentTarget.css('background-color', '');
+	// 	currentTarget = null;
+	// }
+});
--- main.js
+++ main.js
@@ -1,98 +1,113 @@
-var electron = require('electron');
-const {
-	app,
-	BrowserWindow,
-	ipcMain,
-	shell
-} = electron;
-
 var FS = require('fs'),
 	Path = require('path'),
-	Async = require('async');
-
-var DESKTOP_DIR = '/home/ruslan/Qt';
-// var DESKTOP_DIR = app.getPath('home');
-var THEME_DIR = process.argv[2];
-var FOLDER_ICON = Path.resolve(THEME_DIR, 'places/64/folder.svg');
-
+	Async = require('async'),
+	Electron = require('electron'),
+	isOSX = (process.platform === 'darwin'),
+	{
+		app,
+		BrowserWindow,
+		ipcMain,
+		shell
+	} = Electron;
 
 if (!app.requestSingleInstanceLock()) return app.quit();
 
 
-function createWindow () {
+// var DESKTOP_DIR = '/home/ruslan/Qt';
+var DESKTOP_DIR = app.getPath('home');
+var THEME_DIR = process.argv[2] || '';
+var FOLDER_ICON = Path.resolve(THEME_DIR, 'places/64/folder.svg');
 
-	 var { width, height } = electron.screen.getPrimaryDisplay().bounds
-	 var foo = electron.screen.getPrimaryDisplay().workArea.y;
-	 console.info()
 
-	var mainWindow = new BrowserWindow({
-		x: 0,
-		y: 30,
-		title: 'xxx-yyy-zzz-foo-bar',
-		type: 'desktop',
-		show: false,
-		frame: false,
-		transparent : true,
-		skipTaskbar: true,
-		webPreferences: {
-			webgl: false,
-			webaudio: false,
-			plugins: false,
-			defaultFontSize: 13,
-			defaultFontFamily: {
-				standard: 'Apple Garamond Regular'
-			},
-			additionalArguments: ' --enable-transparent-visuals --disable-gpu'
-		}
+
+
+function getPathIcon(path, ret) {
+	var isDirectory = FS.lstatSync(path).isDirectory();
+	if (isDirectory) return ret(FOLDER_ICON, true);
+	app.getFileIcon(path, {size: 'large'}, function(error, icon) {
+		ret(icon.toDataURL(), false);
 	});
-
-
-	mainWindow.loadURL(`file://${__dirname}/index.html?width=${width}&height=${height-30}`)
-    
-    ipcMain.on('openItem', function(event, fullPath) {
-    	shell.openItem(fullPath);
-	});
-
-	function getPathIcon(path, ret) {
-		var isDirectory = FS.lstatSync(path).isDirectory();
-		if (isDirectory) return ret(FOLDER_ICON, true);
-		app.getFileIcon(path, {size: 'large'}, function(error, icon) {
-			ret(icon.toDataURL(), false);
-		});
-
-	}
-
-
-    mainWindow.webContents.on('did-finish-load', function() {
-    	FS.readdir(DESKTOP_DIR, function(error, files) {
-    		var resultFiles = [];
-    		Async.each(files, function(fileName, next) {
-
-    			if (fileName[0] === '.') return next();
-    			var filePath = Path.resolve(DESKTOP_DIR, fileName);
-
-
-    			getPathIcon(filePath, function(icon, isDirectory) {
-	    			resultFiles.push({
-	    				icon: icon,
-	    				path: filePath,
-	    				name: fileName,
-	    				isDirectory: isDirectory
-	    			});
-	    			next();
-    			})
-
-
-
-    		}, function() {
-    			mainWindow.webContents.send('renderDesktop', resultFiles);
-    		});
-    	});
-        
-
-
-    });
-
 }
 
-app.on('ready', () => setTimeout(createWindow, 300));
+function readDesktop(ret) {
+	var resultFiles = [];
+	FS.readdir(DESKTOP_DIR, function(error, files) {
+		Async.each(files, function(fileName, next) {
+
+			if (fileName[0] === '.') return next();
+			var filePath = Path.resolve(DESKTOP_DIR, fileName);
+
+
+			getPathIcon(filePath, function(icon, isDirectory) {
+				resultFiles.push({
+					icon: icon,
+					path: filePath,
+					name: fileName,
+					isDirectory: isDirectory
+				});
+				next();
+			})
+
+
+
+		}, function() {
+			ret(null, resultFiles);
+		});
+	});
+}
+
+function createWindow(ret) {
+	//setTimeout(createWindow, 300)
+	app.on('ready', function() {
+
+		var { width, height } = Electron.screen.getPrimaryDisplay().bounds
+		var foo = Electron.screen.getPrimaryDisplay().workArea.y;
+
+		var mainWindow = new BrowserWindow(Object.assign({
+			x: 0,
+			y: 30,
+			
+			title: 'xxx-yyy-zzz-foo-bar',
+			type: 'desktop',
+			show: false,
+			frame: false,
+			transparent : true,
+			skipTaskbar: true,
+
+			webPreferences: {
+				webgl: false,
+				webaudio: false,
+				plugins: false,
+				defaultFontSize: 13,
+				defaultFontFamily: {
+					standard: 'Apple Garamond Regular'
+				},
+				additionalArguments: ' --enable-transparent-visuals --disable-gpu'
+			}
+		}, isOSX && {
+			type: 'normal',
+			frame: true,
+			transparent: false,
+			skipTaskbar: false
+		}));
+
+		mainWindow.loadURL(`file://${__dirname}/assets/index.html?width=${width}&height=${height-30}`);
+
+		mainWindow.webContents.on('did-finish-load', function() {
+			ret(null, mainWindow);
+		});
+
+	});
+}
+
+
+Async.parallel({
+	files: readDesktop,
+	mainWindow: createWindow
+}, function(error, result) {
+	result.mainWindow.webContents.send('renderDesktop', result.files);
+});
+
+ipcMain.on('openItem', function(event, fullPath) {
+	shell.openItem(fullPath);
+});
